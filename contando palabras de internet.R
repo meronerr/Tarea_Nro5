d8c7501640d04df6af204ec3e08e9c7848671993
Matias Contreras <matias.contrerasf@utem.cl>
2019-01-05 11:56:34 -0300

    pagina final

--- contando palabras de internet.R
+++ contando palabras de internet.R
@@ -14,22 +14,49 @@ contenidoweb<-html_nodes(webpage,'.justificado')
 textoNoticia<-html_text(contenidoweb)
 #viendo a priori la info en la variable textoNoticia
 print(textoNoticia)
-textoNoticia<-gsub("\n"," ", textoNoticia)
-textoNoticia<-gsub("\t"," ", textoNoticia)
-textoNoticia<-gsub("\u0080"," ", textoNoticia)
-textoNoticia<-gsub("\u009d"," ", textoNoticia)
+textoNoticia<-gsub("\n","", textoNoticia)
+textoNoticia<-gsub("\t","", textoNoticia)
+textoNoticia<-gsub("\u0080","", textoNoticia)
+textoNoticia<-gsub("\u009d","", textoNoticia)
 print(textoNoticia)
-ll,textoNoticia<-gsub("â","", textoNoticia)
-textoNoticia<-gsub("Ã©","é", textoNoticia)
+textoNoticia<-gsub("â\u009c","", textoNoticia)
+textoNoticia<-gsub("Ãº","ú", textoNoticia)
+textoNoticia<-gsub("Ã¡","é", textoNoticia)
+textoNoticia<-gsub("Ã","í", textoNoticia)
+textoNoticia<-gsub("Ã³","ó ", textoNoticia)
+textoNoticia<-gsub("Ã©","é ", textoNoticia)
+textoNoticia<-gsub("â"," ", textoNoticia)
+textoNoticia<-gsub("í³","ó", textoNoticia)
+textoNoticia<-gsub("í©","é", textoNoticia)
+textoNoticia<-gsub("[.]"," ", textoNoticia)
+textoNoticia<-gsub(",","", textoNoticia)
+textoNoticia<-gsub("[(]","", textoNoticia)
+textoNoticia<-gsub("[)]","", textoNoticia)
 
-textoNoticia<-gsub("Ã³","ó", textoNoticia)
-textoNoticia<-gsub("Ã","í ", textoNoticia)
-textoNoticia<-gsub("á","á ", textoNoticia)
-textoNoticia<-gsub("s","s ", textoNoticia)
-textoNoticia<-gsub(""," ", textoNoticia)
 splitEspacioNoticia<-strsplit(textoNoticia, " ")[[1]]
-splitEspacioNoticia<-tolower(splitEspacioNoticia)
-unlistnoticias<-unlist(splitEspacioNoticia)
-tablapalabras<-table(unlistnoticias)
-dfPalabrasNoticia<-as.data.frame(tablapalabras)
 
+splitEspacioNoticia<-tolower(splitEspacioNoticia)
+
+unlistnoticias<-unlist(splitEspacioNoticia)
+
+tablapalabras<-table(unlistnoticias)
+
+dfPalabrasNoticia<-as.data.frame(tablapalabras)
+#almacenando la información en CSV
+write.csv(dfPalabrasNoticia, file="PalabrasNoticia.csv")
+#almacenando la información en txt
+
+#tabla
+#extraer contenido de una tabla a traves de tag table
+tablaProductos<-html_nodes(webpage,".productos")
+#extraer los elementos de la tabla
+contenedordeTablas<-html_nodes(tablaProductos,"table")
+#viendo el contenido de la posición 1,2 de la tabla
+
+tabla1<-html_table(contenedordeTablas[1][[1]])
+
+tabla2<-html_table(contenedordeTablas[2][[1]])
+sprint(tabla1)
+contenedordeTablas<-html_nodes((tablaProductos,""))
+
+install.packages("ggplot2")
